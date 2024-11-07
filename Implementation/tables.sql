50d995b3d7c811db614244b72c1245761cbe3189
Henry <henrykroeth@gmail.com>
2024-11-06 20:50:39 -0500

    file name correction

---
+++ Implementation/tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Suppliers (
+    supplier_id INT PRIMARY KEY,
+    supplier_name varchar(255),
+    contact_info varchar(255)
+);
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    order_date DATE,
+    supplier_id INT,
+    total_amount DECIMAL,
+    FOREIGN KEY (supplier_id) REFERENCES Suppliers(supplier_id)
+);
