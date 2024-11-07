53de90fba2f653a2b4aadd3ad85b27c7a6046474
Max Sloan <104081925+maxsloan44@users.noreply.github.com>
2024-11-06 20:47:05 -0500

    Update Tables.sql

--- Implementation/Tables.sql
+++ Implementation/Tables.sql
@@ -1,9 +1,11 @@
+-- Table made by Max
 CREATE TABLE Suppliers (
     supplier_id INT PRIMARY KEY,
     supplier_name varchar(255),
     contact_info varchar(255)
 );
 
+-- Table made by Max
 CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     order_date DATE,
