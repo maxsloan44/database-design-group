78058e874aefb6884d7db1f1643063d3f9300b87
Max Sloan <104081925+maxsloan44@users.noreply.github.com>
2024-11-07 19:19:32 -0500

    Update tables.sql
    Fixed some stuff:
    added "if not exists" to prevent errors if tables exist
    added column declarations for foreign keys (the column must be declared/created before a FOREIGN KEY line is used, fixed that for Henry's two tables)

--- Implementation/tables.sql
+++ Implementation/tables.sql
@@ -1,12 +1,12 @@
 -- Table made by Max
-CREATE TABLE Suppliers (
+CREATE TABLE IF NOT EXISTS Suppliers (
     supplier_id INT PRIMARY KEY,
     supplier_name varchar(255),
     contact_info varchar(255)
 );
 
 -- Table made by Max
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     order_date DATE,
     supplier_id INT,
@@ -15,33 +15,36 @@ CREATE TABLE Orders (
 );
 
 -- Table made by Henry
-CREATE TABLE Products (
+CREATE TABLE IF NOT EXISTS Products (
     product_id INT PRIMARY KEY,
     product_name VARCHAR(50),
     category VARCHAR(50),
     price DECIMAL,
     quantity_in_stock INT,
+    supplier_id INT,
     FOREIGN KEY (supplier_id) REFERENCES Suppliers(supplier_id)
 );
 
 -- Table made by Henry
-CREATE TABLE OrderDetails (
+CREATE TABLE IF NOT EXISTS OrderDetails (
      detail_id INT PRIMARY KEY,
      quantity_ordered INT,
      price_at_order_time DECIMAL,
+     order_id INT,
+     product_id INT,
      FOREIGN KEY (order_id) REFERENCES Orders(order_id),
      FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
 -- Table made by Cass
-CREATE TABLE InventoryReport (
+CREATE TABLE IF NOT EXISTS InventoryReport (
 	report_id INT PRIMARY KEY,
 	report_date DATE,
 	report_data varchar(255)
 );
 
 -- Table made by Cass
-CREATE TABLE User (
+CREATE TABLE IF NOT EXISTS User (
 	user_id INT PRIMARY KEY,
 	username varchar(255),
 	password varchar(255),
