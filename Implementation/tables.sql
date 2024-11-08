0f504d8e7bbca940aa2306f97e27a76d810fbbd6
Max Sloan <104081925+maxsloan44@users.noreply.github.com>
2024-11-07 19:15:27 -0500

    Update tables.sql
    fixed/changed spacing

--- Implementation/tables.sql
+++ Implementation/tables.sql
@@ -16,7 +16,7 @@ CREATE TABLE Orders (
 
 -- Table made by Henry
 CREATE TABLE Products (
-	product_id INT PRIMARY KEY,
+    product_id INT PRIMARY KEY,
     product_name VARCHAR(50),
     category VARCHAR(50),
     price DECIMAL,
@@ -26,11 +26,11 @@ CREATE TABLE Products (
 
 -- Table made by Henry
 CREATE TABLE OrderDetails (
-	detail_id INT PRIMARY KEY,
-	quantity_ordered INT,
-	price_at_order_time DECIMAL,
-	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-	FOREIGN KEY (product_id) REFERENCES Products(product_id)
+     detail_id INT PRIMARY KEY,
+     quantity_ordered INT,
+     price_at_order_time DECIMAL,
+     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
 -- Table made by Cass
@@ -40,7 +40,7 @@ CREATE TABLE InventoryReport (
 	report_data varchar(255)
 );
 
---Table made by Cass
+-- Table made by Cass
 CREATE TABLE User (
 	user_id INT PRIMARY KEY,
 	username varchar(255),
